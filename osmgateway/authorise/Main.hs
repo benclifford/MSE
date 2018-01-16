7bc139403f2844163217586bae9d12e20ad161c7
Ben Clifford <benc@hawaga.org.uk>
2018-01-16 17:40:59 +0000

    osmgateway-authorise is able to authorise against OSM

--- osmgateway/authorise/Main.hs
+++ osmgateway/authorise/Main.hs
@@ -1,6 +1,47 @@
+{-# Language OverloadedStrings #-}
 module Main where
+
+import Control.Lens
+import Data.Text (pack)
+import Network.Wreq
+import System.Environment (getArgs)
 
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "MSE/OSM gateway authorise"
+  args <- getArgs
+  let apiId = args !! 0
+  let token = args !! 1
+  let email = args !! 2
+  let password = args !! 3
+  putStrLn $ "API ID: " ++ apiId
+  putStrLn $ "Token: " ++ token
+  putStrLn $ "Email: " ++ email
+  putStrLn $ "Password: " ++ password
+
+  -- now query users.php?action=authorise&...etc...
+  -- with parts an array:
+  --    password
+  --    email
+
+  -- presumably token and api ID are globals in the PHP code.
+  -- we'l get back a userid and a secret, and those will need to
+  -- be supplied as parameters in future along with apiid and token.
+  
+  let url = "https://www.onlinescoutmanager.co.uk/users.php"
+
+  let opts = defaults & param "action" .~ ["authorise"]
+
+  let postData = [ "apiid" := apiId
+                 , "token" := token
+                 , "password" := password
+                 , "email" := email
+                 ]
+ 
+  r <- postWith opts url postData
+
+  print r
+
+  print $ r ^.. responseBody
