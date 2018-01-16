62cf6a670d1c8f654a4c71d03cb0e6aef1161c0c
Ben Clifford <benc@hawaga.org.uk>
2018-01-16 18:44:48 +0000

    can read sections and terms from OSM API

--- osmgateway/app/Main.hs
+++ osmgateway/app/Main.hs
@@ -1,6 +1,80 @@
+{-# Language OverloadedStrings #-}
 module Main where
+
+import Control.Lens
+import Network.Wreq
 
 import Lib
 
+data Secrets = Secrets {
+   _userid :: String,
+   _secret :: String,
+   _apiId :: String,
+   _token :: String
+  } deriving (Read, Show)
+
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "osmgateway"
+
+
+  putStrLn "authorisation secrets:"
+  secrets <- read <$> readFile "secrets.dat" :: IO Secrets
+  print secrets
+
+
+  -- this gives a list of terms, but that's not what I want
+  -- ... I want section info. I can grab section IDs out of
+  -- this at least, to iterate over.
+
+  putStrLn "osmgateway: getting list of terms"
+  
+  let url = "https://www.onlinescoutmanager.co.uk/api.php"
+
+  let opts = defaults & param "action" .~ ["getTerms"]
+
+  let postData = [ "userid" := _userid secrets
+                 , "secret" := _secret secrets  
+                 , "apiid" := _apiId secrets
+                 , "token" := _token secrets
+                 ]
+
+{-
+                 , "password" := password
+                 , "email" := email
+                 ]
+-}
+
+  r <- postWith opts url postData
+
+  print r
+
+  print $ r ^.. responseBody
+
+  putStrLn "osmgateway: getting user roles"
+  
+  let url = "https://www.onlinescoutmanager.co.uk/api.php"
+
+  let opts = defaults & param "action" .~ ["getUserRoles"]
+
+  let postData = [ "userid" := _userid secrets
+                 , "secret" := _secret secrets  
+                 , "apiid" := _apiId secrets
+                 , "token" := _token secrets
+                 ]
+
+{-
+                 , "password" := password
+                 , "email" := email
+                 ]
+-}
+
+  r <- postWith opts url postData
+
+  print r
+
+  print $ r ^.. responseBody
+
+
+
+  putStrLn "osmgateway finished"
