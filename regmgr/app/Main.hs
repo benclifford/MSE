6cdf16db32eeae32d7291533cb292ea1e8cfbced
Ben Clifford <benc@hawaga.org.uk>
2018-01-27 15:19:57 +0000

    implement HTML form instead of static display, but no update endpoint

--- regmgr/app/Main.hs
+++ regmgr/app/Main.hs
@@ -3,10 +3,24 @@
 {-# Language OverloadedStrings #-}
 {-# Language ScopedTypeVariables #-}
 
+-- blaze tutorial here:
+-- https://jaspervdj.be/blaze/tutorial.html
+
 module Main where
 
+-- QUESTION/DISCUSSION: rules for imports that I should follow but don't:
+-- Imports may only be qualified, or import explicitly named things
+-- The intention is that looking at any part of the source code, it
+-- should be possible to figure out where a particular symbol has
+-- come from by observing the import list.
+-- Generally operators will need to be explicitly imported so that
+-- they can be nicely used as operators without a package name.
 
 import Control.Monad.IO.Class (liftIO)
+
+import Data.String (fromString)
+
+import Data.Monoid ( (<>) )
 
 import Database.PostgreSQL.Simple as PG
 import Database.PostgreSQL.Simple.Time as PG
@@ -18,6 +32,8 @@ import Servant.API
 import Servant.HTML.Blaze as SB
 
 import qualified Text.Blaze.Html5 as B
+import Text.Blaze.Html5 ( (!) )
+import qualified Text.Blaze.Html5.Attributes as BA
 
 import Lib
 
@@ -27,7 +43,7 @@ type PingAPI =
 type HTMLPingAPI =
   "htmlping" :> Get '[SB.HTML] B.Html
 
-type InboundAuthenticatorAPI = "inbound" :> Capture "uuid" String :> Get '[PlainText] String
+type InboundAuthenticatorAPI = "inbound" :> Capture "uuid" String :> Get '[HTML] B.Html
 
 type API = PingAPI :<|> InboundAuthenticatorAPI
       :<|> HTMLPingAPI
@@ -47,7 +63,7 @@ handleHTMLPing = return $ B.docTypeHtml $ do
     B.p "ok"
 
 
-handleInbound :: String -> Handler String
+handleInbound :: String -> Handler B.Html
 handleInbound authenticator = do
   liftIO $ putStrLn $ "handleInbound called, authenticator=" ++ authenticator
 
@@ -78,13 +94,70 @@ handleInbound authenticator = do
 
   entry :: [(Integer, String, String, PG.ZonedTimestamp, String, String, String)] <- liftIO $ query conn "SELECT * FROM regmgr_attendee WHERE authenticator=?" [authenticator]
 
+  let (n, authenticator, state, modified, firstname, lastname, dob) = head entry -- assumes exactly one entry matches this authenticator. BUG: there might be none;  there might be more than 1 but that is statically guaranteed not to happen in the SQL schema (so checked by the SQL type system, not the Haskell type system) - so that's an 'error "impossible"' case.
+
   liftIO $ putStrLn $ "got sql result: " ++ show entry
 
   liftIO $ putStrLn "closing db"
   liftIO $ PG.close conn 
 
+  let title = "Registration for " <> B.toHtml firstname <> " " <> B.toHtml lastname
+
+  let editableHtml =
+        B.docTypeHtml $ do
+          B.head $ do
+            B.title title
+          B.body $ do
+            B.h1 title
+            B.p "Please fill out this registration form. We have put information that we know already into the form, but please check and correct those if that information is wrong."
+            -- QUESTION/DISCUSSION: type_ has to have a different name with an underscore because type is a reserved word.
+            B.form ! BA.action "/updateform"
+                   ! BA.method "post"
+             $ do
+
+              -- QUESTION/DISCUSSION: the authenticator lets us find the relevant record (it's a combo of identifier and authenticator...) and the modified value lets us do OCC transaction handling.
+              B.input
+                  ! BA.type_ "hidden"
+                  ! BA.name "authenticator"
+                  ! BA.value (fromString authenticator)
+              B.input
+                  ! BA.type_ "hidden"
+                  ! BA.name "modified"
+                  ! BA.value (fromString (show modified)) -- might need a better data format to be able to deserialise it elsewhere.
+
+              B.p $ do
+                "Family name: "
+                B.input
+                  ! BA.type_ "text" 
+                  ! BA.name "firstname" 
+                  ! BA.value (fromString firstname)
+              B.p $ do
+                "Family name: " 
+                B.input
+                  ! BA.type_ "text"
+                  ! BA.name "lastname"
+                  ! BA.value (fromString lastname)
+              -- QUESTION/DISCUSSION: this could be a date picker on the client side in javascript?
+              B.p $ do
+                "Date of birth: "
+                B.input
+                  ! BA.type_ "text"
+                  ! BA.name "dob"
+                  ! BA.value (fromString dob)
+              B.input
+                ! BA.type_ "submit"
+                ! BA.value "Register for event"
+            B.hr
+            B.h2 "Internal debugging information"
+            B.p $ "Last modified: " <> B.toHtml (show modified)
+            B.p $ "State: " <> B.toHtml state
+            B.p $ "Record number: " <> B.toHtml n
+
   liftIO $ putStrLn "end of req"
-  return $ "authenticator: " ++ authenticator ++ "/ " ++ show entry
+
+  return editableHtml
+
+  -- return $ "authenticator: " <> authenticator ++ "/ " ++ show entry
 
 
 regmgrAPI :: Proxy API
