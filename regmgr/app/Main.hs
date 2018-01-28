9532a35663405dd7be2ec69d968c968b659810ce
Ben Clifford <benc@hawaga.org.uk>
2018-01-28 00:17:17 +0000

    link PDF from main UI flow

--- regmgr/app/Main.hs
+++ regmgr/app/Main.hs
@@ -164,7 +164,13 @@ regformHtml :: String -> DF.View B.Html -> Bool -> B.Html
 regformHtml auth view editable = do
             if editable
               then B.p "Please fill out this registration form. We have put information that we know already into the form, but please check and correct those if that information is wrong."
-              else B.p "This is the information held for this attendee. Please contact your section leader if any of this information is incorrect."
+              else do
+                B.p "This is the information held for this attendee. Please contact your section leader if any of this information is incorrect."
+                B.p $ do
+                  "If you have not done so already, "
+                  (B.a ! BA.href ("/pdf/" <> fromString auth))
+                    "please print out and sign the permission form"
+                  "."
 
             -- QUESTION/DISCUSSION: type_ has to have a different name with an underscore because type is a reserved word.
             B.form ! BA.action ("/register/" <> (fromString auth))
@@ -299,9 +305,15 @@ handleUpdateForm auth reqBody = do
                 B.title title
               B.body $ do
                 B.h1 title
+                B.p $ do
+                  "Next steps: "
+                  (B.a ! BA.href ("/pdf/" <> fromString auth))
+                    "please print out and sign the permission form"
+                  ", and then give it to a leader with payment."
                 B.p "debug: handleUpdateForm - there are no errors"
-                B.p "next steps: print PDF (link here), sign and give to a leader with payment"
-                B.p $ B.toHtml $ show val
+                B.p $ do
+                  "debug: "
+                  B.toHtml $ show val
           else -- failure due to database error
             return $ B.docTypeHtml $ do
               B.head $ do
